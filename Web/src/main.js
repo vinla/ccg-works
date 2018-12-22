89be2a0070e7bcb77b09edbdf23dea6ccff93a9a
Vincent <vinla@hotmail.co.uk>
2018-12-22 14:34:56 +0000

    Added Vuex to manage profile

--- Web/src/main.js
+++ Web/src/main.js
@@ -5,6 +5,7 @@ import auth from './auth';
 import modal from './modal';
 import App from './App.vue';
 import axios from 'axios';
+import store from './store';
 import '@/assets/main.css';
 
 Vue.use(VueResource);
@@ -17,5 +18,6 @@ axios.defaults.baseURL = "http://localhost:5000/api";
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
